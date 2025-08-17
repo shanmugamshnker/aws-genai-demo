1521256f18c98baaca15c35c5a7c91b63d9d7ee0
Shan <63295855+shanmugamshnker@users.noreply.github.com>
2025-08-17 19:53:23 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,87 +1,27 @@
-# Configure the AWS Provider
 provider "aws" {
   region = "us-west-2"
 }
 
-# Create a VPC
-resource "aws_vpc" "main" {
-  cidr_block           = "10.0.0.0/16"
-  enable_dns_hostnames = true
-  enable_dns_support   = true
+resource "aws_s3_bucket" "insecure_bucket" {
+  bucket = "my-insecure-bucket"
+  acl    = "public-read" 
 
-  tags = {
-    Name = "main-vpc"
-    Environment = "Production"
+  server_side_encryption_configuration {
   }
 }
 
-# Create a public subnet
-resource "aws_subnet" "public" {
-  vpc_id            = aws_vpc.main.id
-  cidr_block        = "10.0.1.0/24"
-  availability_zone = "us-west-2a"
-
-  tags = {
-    Name = "Public Subnet"
-  }
-}
-
-# Create an Internet Gateway
-resource "aws_internet_gateway" "main" {
-  vpc_id = aws_vpc.main.id
-
-  tags = {
-    Name = "Main IGW"
-  }
-}
-
-# Create a security group
-resource "aws_security_group" "allow_ssh" {
-  name        = "allow_ssh"
-  description = "Allow SSH inbound traffic"
-  vpc_id      = aws_vpc.main.id
+resource "aws_security_group" "open_sg" {
+  name        = "open_sg"
+  description = "Allow all traffic"
 
   ingress {
-    description = "SSH from anywhere"
-    from_port   = 22
-    to_port     = 22
-    protocol    = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  egress {
     from_port   = 0
-    to_port     = 0
-    protocol    = "-1"
-    cidr_blocks = ["0.0.0.0/0"]
-  }
-
-  tags = {
-    Name = "allow_ssh"
+    to_port     = 65535
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
   }
 }
 
-# Create an EC2 instance
-resource "aws_instance" "web_server" {
-  ami           = "ami-0c55b159cbfafe1f0"  # Amazon Linux 2 AMI ID
-  instance_type = "t2.micro"
-  subnet_id     = aws_subnet.public.id
-
-  vpc_security_group_ids = [aws_security_group.allow_ssh.id]
-
-  tags = {
-    Name = "Web Server"
-    Environment = "Production"
-  }
-
-  root_block_device {
-    volume_size = 8
-    volume_type = "gp2"
-  }
-}
-
-# Output the instance public IP
-output "instance_public_ip" {
-  value = aws_instance.web_server.public_ip
-  description = "Public IP of the web server"
+variable "db_password" {
+  default = "SuperSecret123" 
 }
